bd3a2f916ca7fa869eb45470baba2ac956ea4720
ganeshpandi-5032 <157736256+ganeshpandi-5032@users.noreply.github.com>
2024-01-25 23:37:58 +0530

    Create Army Game.py

---
+++ Army Game.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gameWithCells' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def gameWithCells(n, m):
